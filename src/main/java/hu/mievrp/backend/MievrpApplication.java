fd63ff03de30127a631fdef56285973d5e9a3100
Godfrid <godfrid@gmail.com>
2018-11-13 15:07:57 +0100

    init spring

---
+++ src/main/java/hu/mievrp/backend/MievrpApplication.java
@@ -0,0 +1,12 @@
+package hu.mievrp.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MievrpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MievrpApplication.class, args);
+	}
+}
